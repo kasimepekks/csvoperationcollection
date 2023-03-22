25500e51593a77baae9b76fe3693214c00a1a4d0
kasime <273825908@qq.com>
2023-03-22 14:36:37 +0800

    第一次提交V1

---
+++ ConsoleExcute.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSV文件操作集合程序
+{
+    public static class ConsoleExcute
+    {
+        public static  void consoleRecurrence()
+        {
+            try
+            {
+                Console.WriteLine("请选择一个操作模式：");
+                Console.WriteLine("1. 结构化数据库CSV拆分");
+                Console.WriteLine("2. 合并CSV-时间模式");
+                Console.WriteLine("3. 合并CSV-txt模式");
+                Console.WriteLine("4. 去除CSV头部信息");
+                Console.WriteLine("********************请输入序号并按回车********************\"");
+                string? yesorno = "n";
+                int value = 0;
+                while (yesorno != "y")
+                {
+                    var index = Console.ReadLine();
+                    if (int.TryParse(index, out value))
+                    {
+                        if (value > 0 && value < 5)
+                        {
+                            Console.WriteLine($"你输入的序号为{index}，确定执行吗？确定请输入y，取消请输入任意其他");
+                            yesorno = Console.ReadLine()?.ToLower();
+                            if (yesorno == "y")
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("请重新输入序号");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("你输入的数字有误，请重新输入：");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("你输入的不是数字请重新输入：");
+                    }
+                }
+                if (yesorno == "y")
+                {
+                   var t= Task.Run(() => {
+                       Console.WriteLine($"开始执行序号为{value}的操作……");
+                       switch (value)
+                       {
+                           case 1:
+                               {
+                                   ExcuteOperation.excuteFirst();
+                               }
+                               break;
+                           case 2:
+                               {
+                                   ExcuteOperation.excuteSecond();
+                               }
+                               break;
+                           case 3:
+                               {
+                                   ExcuteOperation.excuteThird();
+                               }
+                               break;
+                           case 4:
+                               {
+                                   ExcuteOperation.excuteFourth();
+                               }
+                               break;
+                       }
+
+                   });
+                    t.GetAwaiter().OnCompleted(() =>
+                    {
+                        consoleRecurrence();
+                    });
+
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message); ;
+            }
+        }
+    }
+}
---
+++ ExcuteOperation.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YXH_Tools_Files.Tools_Configure;
+using YXH_Tools_Files.Tools_DirectoryandFile;
+using YXH_Tools_Files.Tools_Excute;
+
+namespace CSV文件操作集合程序
+{
+    public static class ExcuteOperation
+    {
+        /// <summary>
+        /// 执行第一个操作
+        /// </summary>
+        public static void excuteFirst()
+        {
+            var orignaldatapath = XMLConfigure.YXHGetXMLConfigSection("结构化数据库CSV拆分", "orignaldatapath");
+            var conditionmodelpath = XMLConfigure.YXHGetXMLConfigSection("结构化数据库CSV拆分", "conditionmodelpath");
+            var savedatapath = XMLConfigure.YXHGetXMLConfigSection("结构化数据库CSV拆分", "savedatapath");
+            var saveconditionpath = XMLConfigure.YXHGetXMLConfigSection("结构化数据库CSV拆分", "saveconditionpath");
+            var standard = XMLConfigure.YXHGetXMLConfigSection("结构化数据库CSV拆分", "standard");
+            var phase = XMLConfigure.YXHGetXMLConfigSection("结构化数据库CSV拆分", "phase");
+            var ballast = XMLConfigure.YXHGetXMLConfigSection("结构化数据库CSV拆分", "ballast");
+            var selectedcollumn = XMLConfigure.YXHGetXMLConfigSection("结构化数据库CSV拆分", "selectedcollumn");
+            if (standard != null && phase != null && ballast != null && selectedcollumn != null)
+            {
+                var standardarray = standard.Split(",");
+                var phasearray = phase.Split(",");
+                var ballastarray = ballast.Split(",");
+                var selectedcollumnarray = selectedcollumn.Split(",");
+                if (standardarray != null && phasearray != null && ballastarray != null
+               && selectedcollumnarray != null && orignaldatapath != null
+               && conditionmodelpath != null && savedatapath != null && saveconditionpath != null)
+                {
+                    orignaldatapath=orignaldatapath.YXHPathReplace();
+                    conditionmodelpath = conditionmodelpath.YXHPathReplace();
+                    savedatapath = savedatapath.YXHPathReplace();
+                    saveconditionpath = saveconditionpath.YXHPathReplace();
+                    Tools_Excute_CSVSplitforDatabase.YXHExcuteCSVSplit(orignaldatapath, selectedcollumnarray, standardarray, phasearray, ballastarray, conditionmodelpath, savedatapath, saveconditionpath);
+                }
+                else
+                {
+                    Console.WriteLine("配置文件填写有误，请检查！");
+                }
+            }
+            else
+            {
+                Console.WriteLine("配置文件填写有误，请检查！");
+            }
+        }
+        public static void excuteSecond()
+        {
+            var orignaldatapath = XMLConfigure.YXHGetXMLConfigSection("合并CSV-时间模式", "orignaldatapath");
+            var savedatapath = XMLConfigure.YXHGetXMLConfigSection("合并CSV-时间模式", "savedatapath");
+            var selectdatefile = XMLConfigure.YXHGetXMLConfigSection("合并CSV-时间模式", "selectdatefile");
+            if(selectdatefile != null)
+            {
+                var selectdatefilearray = selectdatefile.Split('t');
+                if(orignaldatapath!=null && savedatapath!=null && selectdatefilearray != null)
+                {
+                    orignaldatapath = orignaldatapath.YXHPathReplace();
+                    savedatapath = savedatapath.YXHPathReplace();
+                    Tools_Excute_CSVCombine.YXHExcuteCombination(orignaldatapath, savedatapath, selectdatefilearray, "1");
+                }
+                else
+                {
+                    Console.WriteLine("配置文件填写有误，请检查！");
+                }
+            }
+            else
+            {
+                Console.WriteLine("配置文件填写有误，请检查！");
+            }
+        }
+        public static void excuteThird()
+        {
+            var orignaldatapath = XMLConfigure.YXHGetXMLConfigSection("合并CSV-txt模式", "orignaldatapath");
+            var savedatapath = XMLConfigure.YXHGetXMLConfigSection("合并CSV-txt模式", "savedatapath");
+            var selectdatefile = XMLConfigure.YXHGetXMLConfigSection("合并CSV-txt模式", "selectdatefile");
+            if (selectdatefile != null)
+            {
+                var selectdatefilearray = selectdatefile.Split('t');
+                if (orignaldatapath != null && savedatapath != null && selectdatefilearray != null)
+                {
+                    orignaldatapath = orignaldatapath.YXHPathReplace();
+                    savedatapath = savedatapath.YXHPathReplace();
+                    Tools_Excute_CSVCombine.YXHExcuteCombination(orignaldatapath, savedatapath, selectdatefilearray, "0");
+                }
+                else
+                {
+                    Console.WriteLine("配置文件填写有误，请检查！");
+                }
+            }
+            else
+            {
+                Console.WriteLine("配置文件填写有误，请检查！");
+            }
+        }
+        public static  void excuteFourth()
+        {
+            var orignaldatapath = XMLConfigure.YXHGetXMLConfigSection("去除CSV头部信息", "orignaldatapath");
+            var savedatapath = XMLConfigure.YXHGetXMLConfigSection("去除CSV头部信息", "savedatapath");
+            if (orignaldatapath != null && savedatapath != null)
+            {
+                orignaldatapath = orignaldatapath.YXHPathReplace();
+                savedatapath = savedatapath.YXHPathReplace();
+                Tools_Excute_HeaderEdit_for_NcodeCSV.YXHExcuteHeaderAsync(orignaldatapath,savedatapath);
+            }
+            else
+            {
+                Console.WriteLine("配置文件填写有误，请检查！");
+            }
+        }
+
+    }
+}
